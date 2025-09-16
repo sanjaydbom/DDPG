344a41203136815b80f7a9686db32bffe257b818
sanjaydbom <64293324+sanjaydbom@users.noreply.github.com>
2025-09-16 12:26:58 -0700

    Create Training Script

--- Architecture.py
+++ Architecture.py
@@ -1,15 +1,15 @@
 import torch
 from torch import nn
 
-from OlensteinUlenbeckProcess import OUP
 
 class Actor(nn.Module):
-    def __init__(self, obs_space, action_space, hidden_layers):
+    def __init__(self, obs_space, action_space, hidden_layers, TAU):
         super(Actor, self).__init__()
         self.head = nn.Linear(obs_space, hidden_layers[0])
         self.hidden = nn.ModuleList([nn.Linear(hidden_layers[i], hidden_layers[i+1]) for i in range(len(hidden_layers)-1)])
         self.tail = nn.Linear(hidden_layers[-1], action_space)
         self.relu = nn.ReLU()
+        self.tau = TAU
 
     def forward(self, state):
         x = self.head(state)
@@ -21,13 +21,18 @@ class Actor(nn.Module):
 
         return x
     
+    def update(self, actor):
+        for target_param, param in zip(self.parameters(), actor.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+    
 class Critic(nn.Module):
-    def __init__(self, obs_space, action_space, hidden_layers):
+    def __init__(self, obs_space, action_space, hidden_layers, TAU):
         super(Critic, self).__init__()
         self.head = nn.Linear(obs_space+action_space, hidden_layers[0])
         self.hidden = nn.ModuleList([nn.Linear(hidden_layers[i], hidden_layers[i+1]) for i in range(len(hidden_layers)-1)])
         self.tail = nn.Linear(hidden_layers[-1], 1)
         self.relu = nn.ReLU()
+        self.tau = TAU
 
     def forward(self, state, action):
         x = torch.cat((state, action), dim = -1)
@@ -40,11 +45,15 @@ class Critic(nn.Module):
 
         return x
     
-def get_architecture(obs_space, action_space, actor_hidden, critic_hidden):
-    actor = Actor(obs_space,actor_hidden,actor_hidden)
-    target_actor = Actor(obs_space,actor_hidden,actor_hidden)
+    def update(self, critic):
+        for target_param, param in zip(self.parameters(), critic.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+    
+def get_architecture(obs_space, action_space, actor_hidden, critic_hidden, tau):
+    actor = Actor(obs_space,actor_hidden,actor_hidden, None)
+    target_actor = Actor(obs_space,actor_hidden,actor_hidden, tau)
     target_actor.load_state_dict(actor.state_dict())
-    critic = Critic(obs_space,action_space,critic_hidden)
-    target_critic = Critic(obs_space,action_space,critic_hidden)
+    critic = Critic(obs_space,action_space,critic_hidden, None)
+    target_critic = Critic(obs_space,action_space,critic_hidden, tau)
     target_critic.load_state_dict(critic.state_dict())
     return actor, target_actor, critic, target_critic
